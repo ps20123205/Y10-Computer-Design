dbf9a8a84c262719932081b0736df720bfc65420
ps20123205 <46880466+ps20123205@users.noreply.github.com>
2019-01-30 11:27:46 +0800

    Add files via upload

---
+++ p5jsTutorial1.3-master/sketch.js
@@ -0,0 +1,27 @@
+function setup()
+{
+  createCanvas(700,500);
+  
+}
+
+function draw()
+{
+	background(255,0,0)
+
+	fill(0,0,255);
+	stroke(0,0,0);
+	strokeWeight(3);
+
+	text("Hello World",80,30);
+
+	rect(10,10,100,50);
+
+	text("Baby Shark",10,50);
+
+	rect(10,100,100,50);
+
+	fill(255,255,255);
+	
+	text("Donald Trump",10,130);
+
+}

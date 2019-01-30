3329c91a1f1f3b043e48e20901d6e6b5082af465
ps20123205 <46880466+ps20123205@users.noreply.github.com>
2019-01-30 11:39:45 +0800

    Add files via upload

--- p5jsTutorial1.3-master/sketch.js
+++ p5jsTutorial1.3-master/sketch.js
@@ -1,27 +1,36 @@
 function setup()
 {
   createCanvas(700,500);
-  
+
 }
 
 function draw()
 {
 	background(255,0,0)
 
-	fill(0,0,255);
-	stroke(0,0,0);
-	strokeWeight(3);
-
-	text("Hello World",80,30);
-
-	rect(10,10,100,50);
-
-	text("Baby Shark",10,50);
-
-	rect(10,100,100,50);
-
 	fill(255,255,255);
-	
+	stroke(255,204,204);
+	strokeWeight(2);
+
+	text("Hello World",0,10);
+
+  fill(0,0,255);
+  stroke(0,0,0);
+  strokeWeight(3);
+
+	rect(0,20,100,50);
+
+  fill(127,0,255);
+	stroke(0,255,0);
+	strokeWeight(10);
+
+	text("Danny Huang",10,50);
+
+  fill(0,0,255);
+  stroke(0,0,0);
+  strokeWeight(3);
 	text("Donald Trump",10,130);
+  rect(10,100,100,50);
+  fill(255,255,255);
 
 }
